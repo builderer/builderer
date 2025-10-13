c42b158aee13d78529dd502db4516de87a3bc754
James Dolan <jamesdolan@users.noreply.github.com>
2025-10-12 20:45:46 -0700

    licenses dump command (#10)

--- builderer/__main__.py
+++ builderer/__main__.py
@@ -3,6 +3,7 @@ from argparse import ArgumentParser
 from builderer.details.tools.generate import generate_main
 from builderer.details.tools.graph import graph_main
 from builderer.details.tools.sources import sources_main
+from builderer.details.tools.licenses import licenses_main
 from builderer.details.tools.validate import validate_main
 from builderer.details.workspace import Workspace
 
@@ -13,6 +14,7 @@ def main():
         "graph": graph_main,
         "sources": sources_main,
         "validate": validate_main,
+        "licenses": licenses_main,
     }
     parser = ArgumentParser()
     parser.add_argument("command", choices=COMMANDS.keys())
